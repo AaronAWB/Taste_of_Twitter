6899526d98feb6d0b8a25ebd5be20e8fb602b717
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-11-30 18:39:36 -0800

    add organization folders

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav>
+            <li>Home</li>
+            <li>Search</li>
+            <li>Tweets</li>
+        </nav>
+    );
+}
+
+export default Navbar;
