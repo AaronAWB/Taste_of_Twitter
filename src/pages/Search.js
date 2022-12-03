296e12e092194126851454dd8a8459dd91677637
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-03 13:24:04 -0800

    updates to search card styling

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,11 +6,18 @@ const Search = () => {
         <div className='container'>
             <div className='card shadow-sm'>
                 <div className='card-body'>
-                    <h5 className="card-title">Card title</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
+                    <h3 className='card-title mb-3'>
+                        Search
+                    </h3>
+                    <input
+                        type='text'
+                        placeholder='Search by Name or Keyword...'
+                        className='form-control input-lg mb-3'
+                    >
+                    </input>
+                    <button className='btn btn-info form-control'>
+                        Search
+                    </button>
                 </div>
             </div>
         </div>
