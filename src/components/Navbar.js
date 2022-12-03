22b1cddd1723d395c0c5358f9c5241ab4333eb24
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-03 10:52:15 -0800

    add Home, Search, and Tweets pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,13 @@ import './Navbar.css';
 const Navbar = () => {
     return(
         <nav className ='navbar navbar-nav bg-light shadow-sm'>
-            <div className='container-fluid d-flex justify-content: start'>
-                <div className='container'>
-                    <a className='navbar-brand'>Logo</a>
-                </div>
-                <div className ='container d-flex justify-contant: start'>
+            <div className='container-fluid d-flex justify-content-start'>
+                <a className='navbar-brand'>Logo</a>
+                <ul>
                     <li className='nav-item'>Home</li>
                     <li className='nav-item'>Search</li>
                     <li className='nav-item'>Tweets</li>
-                </div>
+                </ul>
             </div>
         </nav>
     );
