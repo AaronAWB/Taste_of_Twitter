f420c93a3f794331e4171a04acea3cbcbcf22b4a
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-03 13:36:49 -0800

    add randow tweet placeholder list

--- src/pages/Tweets.js
+++ src/pages/Tweets.js
@@ -1,6 +1,15 @@
 const Tweets = () => {
-    return (  
-        <h1>This is the Tweets display page.</h1>
+    return (
+        <div>
+            <h1>This is the Tweets display page.</h1>
+            <ul>
+                <li>LeVar Burton</li>
+                <li>John Cleese</li>
+                <li>Andy Sterkowitz</li>
+                <li>Alie Ward</li>
+                <li>TBD</li>
+            </ul>
+        </div>
     );
 }
  
