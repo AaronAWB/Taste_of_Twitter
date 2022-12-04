b4560bb2fec5b8a116d3f234cd9935866cdf2132
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-04 15:08:40 -0800

    add LeVar Burton thumbnail

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -1,16 +1,24 @@
+import Burton from '../assets/images/LeVar_Burton.png'
+
 const Random = () => {
     return (
-        <div className='container'>
-            <h1>This is the Tweets display page.</h1>
-            <ul>
-                <li>LeVar Burton</li>
-                <li>John Cleese</li>
-                <li>Andy Sterkowitz</li>
-                <li>Alie Ward</li>
-                <li>Premier League</li>
-            </ul>
+        <div className='container mt-5'>
+            <img 
+                src={Burton}
+                className='img-thumbnail rounded-circle shadow-sm'
+                alt='LeVar Burton'
+                width='200px'
+                height='200px'
+            ></img>
         </div>
     );
 }
  
 export default Random;
+
+
+// LeVar Burton
+// John Cleese
+// Andy Sterkowitz
+// Alie Ward
+// Premier League
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,28 +3,30 @@ import '../styles/Search.css'
 import Tweet from '../components/Tweet';
 
 const Search = () => {
-    return (  
-        <div className='container'>
-            <div className='card shadow-sm'>
-                <div className='card-body'>
-                    <h3 className='card-title mb-3'>
-                        Search
-                    </h3>
-                    <input
-                        type='text'
-                        placeholder='Search by Name or Keyword...'
-                        className='form-control input-lg mb-3'
-                    >
-                    </input>
-                    <button className='btn btn-info form-control'>
-                        Search
-                    </button>
+    return (
+        <div className='container content-container'> 
+            <div className='container search-container'>
+                <div className='card shadow-sm'>
+                    <div className='card-body'>
+                        <h3 className='card-title mb-3'>
+                            Search
+                        </h3>
+                        <input
+                            type='text'
+                            placeholder='Search by Name or Keyword...'
+                            className='form-control input-lg mb-3'
+                        >
+                        </input>
+                        <button className='btn btn-info form-control'>
+                            Search
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div className ='container results-container'>
                 <Tweet />
             </div>
-        </div>
+        </div> 
     );
 }
  
