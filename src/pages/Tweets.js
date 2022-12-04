e18906fc7718b968d88af10e6f6835f6451038b9
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-04 12:00:08 -0800

    updates to random tweet list

--- src/pages/Tweets.js
+++ src/pages/Tweets.js
@@ -1,13 +1,13 @@
 const Tweets = () => {
     return (
-        <div>
+        <div className='container'>
             <h1>This is the Tweets display page.</h1>
             <ul>
                 <li>LeVar Burton</li>
                 <li>John Cleese</li>
                 <li>Andy Sterkowitz</li>
                 <li>Alie Ward</li>
-                <li>TBD</li>
+                <li>Premier League</li>
             </ul>
         </div>
     );
