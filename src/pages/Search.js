3c3b68ffafbbabb34a2a76be055b2b02d87b2ae4
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-04 11:31:58 -0800

    add dummy tweet to search component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -20,7 +20,23 @@ const Search = () => {
                     </button>
                 </div>
             </div>
+            <div className='card shadow-sm'>
+                <div className ='card-body'>
+                    <h5 className='card-title mb-3'>
+                        Name <span className='text-muted'>@twitter-handle </span>
+                    </h5>
+                    <p className='mb-3'>
+                        Here is the text of the tweet. The text of the Tweet may go on for some time. 
+                        It can be fairly long. Like a paragraph of text. It shoud all fit here.
+                    </p>
+                    <p>
+                        This is where an image would go if there was one.
+                    </p>
+
+                </div>
+            </div>
         </div>
+
     );
 }
  
