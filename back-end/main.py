f100272f907579c3379a81229663b89d3e698ec8
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-18 13:20:02 -0800

    install Axios

--- back-end/main.py
+++ back-end/main.py
@@ -5,9 +5,16 @@ from keys import API_BEARER_TOKEN
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/search', method=['GET'])
+@app.route('/', methods=['GET'])
+def go_home():
+    return './front-end/public/index.html'
+
+@app.route('api/tweets/search', methods=['GET'])
 def search_tweets():
-    
+    return 'Hello World'
+
+@app.route('/')
+
 
 
 if __name__ == '__main__':
