96b76bb4cd29e52d788defcb4ee9d2cf0f9abcff
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-24 11:27:06 -0800

    add styling to Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,16 @@
+import '../styles/Home.css'
+
 const Home = () => {
     return (  
-        <div className='container'>
-            <h1>This is the home page.</h1>
+        <div className='container content-container'>
+            <div className='col'>
+                <div className='container row justify-content-md-center search-container'>
+                    This is where the search text will be.
+                </div>
+                <div className='container row justify-content-md-center random-container'>
+                    This is where the random text will be.
+                </div>
+            </div>
         </div>
     );
 }
