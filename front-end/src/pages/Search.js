229ec2ef67f7a8c40d8299c55d1088f4e56be278
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-24 11:33:50 -0800

    add styling to Search.js

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -12,26 +12,28 @@ const makeSearchRequest = (path) => {
 const Search = () => {
     return (
         <div className='container content-container'> 
-            <div className='container search-container'>
-                <div className='card shadow-sm'>
-                    <div className='card-body'>
-                        <h3 className='card-title mb-3'>
-                            Search
-                        </h3>
-                        <input
-                            type='text'
-                            placeholder='Search by Name or Keyword...'
-                            className='form-control input-lg mb-3'
-                        >
-                        </input>
-                        <button className='btn btn-info form-control' onClick={() => {makeSearchRequest('http://127.0.01.:5000/api/tweets/search')}}>
-                            Search
-                        </button>
+            <div className='col'>
+                <div className='container row justify-content-md-center search-container'>
+                    <div className='card shadow-sm'>
+                        <div className='card-body'>
+                            <h3 className='card-title mb-3'>
+                                Search
+                            </h3>
+                            <input
+                                type='text'
+                                placeholder='Search by Name or Keyword...'
+                                className='form-control input-lg mb-3'
+                            >
+                            </input>
+                            <button className='btn btn-info form-control' onClick={() => {makeSearchRequest('http://127.0.01.:5000/api/tweets/search')}}>
+                                Search
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className ='container results-container'>
-                <Tweet />
+                <div className ='container row justify-content-md-center results-container'>
+                    <Tweet />
+                </div>
             </div>
         </div> 
     );
