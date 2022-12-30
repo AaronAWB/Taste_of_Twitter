a31dd0f2b2c4baab4d216b34e1f23159e2d0fe28
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-29 21:10:48 -0800

    add link from search card on Home page

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../styles/Home.css'
 
 const Home = () => {
@@ -5,18 +6,20 @@ const Home = () => {
         <div className='container content-container'>
             <div className='col'>
                 <div className='container row justify-content-md-center search-container'>
-                    <div className='card homepage-card shadow-sm bg-light mt-2 mb-2'>
-                            <div className='card-body'>
-                                <h3 className='card-title mb-2'>
-                                    Search by Handle or Keyword
-                                </h3>
-                                <p>
-                                    Search all of Twitter by Handle or Keyword! 
-                                    The ten most recent Tweets matching your search will be displayed -
-                                    No need to use Twitter or even have a Twitter account!
-                                </p>
-                            </div>
-                    </div>
+                    <Link to={'/search'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <div className='card homepage-card shadow-sm bg-light mt-2 mb-2'>
+                                <div className='card-body'>
+                                     <h3 className='card-title mb-2'>
+                                        Search by Handle or Keyword
+                                    </h3>
+                                    <p>
+                                        Search all of Twitter by Handle or Keyword! 
+                                        The ten most recent Tweets matching your search will be displayed -
+                                        No need to use Twitter or even have a Twitter account!
+                                    </p>
+                                </div>
+                        </div>
+                    </Link>
                 </div>
                 <div className='container row justify-content-md-center random-container'>
                     <div className='card homepage-card shadow-sm bg-light mt-2 mb-2'>
