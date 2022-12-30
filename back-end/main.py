f5cf745a3f1c09e7b7216906208186c61648681b
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-29 20:19:43 -0800

    merge Backend branch into Main

---
+++ back-end/main.py
@@ -0,0 +1,25 @@
+import requests
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+from keys import API_BEARER_TOKEN
+
+url = 'https://api.twitter.com/1.1/search/tweets.json?q=@levarburton'
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': f'Bearer {API_BEARER_TOKEN}'
+}
+
+app = Flask(__name__)
+api = Api(app)
+cors= CORS(app, resources={r'/api/*': {'origins:': '*'}})
+
+@app.route('/api/tweets/search', methods=['GET'])
+def search_tweets():
+    response = requests.get(url, headers=headers)
+    print(response.json)
+    return response.json
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ front-end/src/pages/Search.js
@@ -0,0 +1,45 @@
+
+import '../styles/Search.css'
+import Tweet from '../components/Tweet';
+import Axios from 'axios'
+
+const makeSearchRequest = (path) => {
+    const searchResults = Axios.get(path);
+    console.log(searchResults)
+    return searchResults
+}
+
+const Search = () => {
+    return (
+        <div className='container content-container'> 
+            <div className='col-6'>
+                <div className='container row justify-content-md-center search-container mt-5 mb-2'>
+                    <div className='card shadow-sm bg-light mt-2 mb-2'>
+                        <div className='card-body'>
+                            <h3 className='card-title mb-2'>
+                                Search
+                            </h3>
+                            <input
+                                type='text'
+                                placeholder='Search by Name or Keyword...'
+                                className='form-control input-lg mb-3'
+                            >
+                            </input>
+                            <button className='btn btn-info form-control' onClick={() => {makeSearchRequest('http://127.0.01.:5000/api/tweets/search')}}>
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className ='container row justify-content-md-center results-container mb-2'>
+                    <Tweet />
+                    <Tweet />
+                    <Tweet />
+                    <Tweet />
+                </div>
+            </div>
+        </div> 
+    );
+}
+ 
+export default Search;
