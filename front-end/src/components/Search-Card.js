f649256d4585e916ff20db2649358eef085601dd
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-12-31 11:28:31 -0800

    add Search-Card component

---
+++ front-end/src/components/Search-Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Search_Card = () => {
+    return(
+        <div className='col-6'>
+            <div className='container row justify-content-md-center search-container mt-5 mb-2'>
+                <div className='card shadow-sm bg-light mt-2 mb-2'>
+                    <div className='card-body'>
+                         <h3 className='card-title mb-2'>
+                            Search
+                        </h3>
+                        <input
+                            type='text'
+                            placeholder='Search by Name or Keyword...'
+                            className='form-control input-lg mb-3'
+                        >
+                        </input>
+                        <button className='btn btn-info form-control' onClick={() => {makeSearchRequest('http://127.0.01.:5000/api/tweets/search')}}>
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Search_Card;
