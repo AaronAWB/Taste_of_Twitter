578a5cab9da3161a061df7bf0059f7d542b52f6b
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-01-01 10:47:00 -0800

    refactor API routes

--- back-end/main.py
+++ back-end/main.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from keys import API_BEARER_TOKEN
 
-url = 'https://api.twitter.com/1.1/search/tweets.json?q=@'
+url = 'https://api.twitter.com/1.1/search/tweets.json?q='
 
 headers = {
     'Content-Type': 'application/json',
@@ -16,19 +16,19 @@ app = Flask(__name__)
 api = Api(app)
 cors= CORS(app, resources={r'/api/*': {'origins:': '*'}})
 
-@app.route('/api/tweets/search/<handle>', methods=['GET'])
-def search_tweets(handle):
-    response = requests.get(url+handle, headers=headers).json()
+@app.route('/api/tweets/handle_search/<handle>', methods=['GET'])
+def search_tweets_handle(handle):
+    response = requests.get(url+'@'+handle, headers=headers).json()
     return response
 
-@app.route('/api/tweets/search/<keyword>', methods=['GET'])
-def search_tweets(keyword):
+@app.route('/api/tweets/keyword_search/<keyword>', methods=['GET'])
+def search_tweets_keyword(keyword):
     response = requests.get(url+keyword, headers=headers).json()
     return response
 
 @app.route('/api/tweets/random/<handle>', methods=['GET'])
-def search_tweets(handle):
-    response = requests.get(url+handle, headers=headers).json()
+def search_tweets_random(handle):
+    response = requests.get(url+'@'+handle, headers=headers).json()
     return response
 
 # @app.errorhandler(404)
@@ -38,3 +38,5 @@ def search_tweets(handle):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# http://127.0.0.1:5000
