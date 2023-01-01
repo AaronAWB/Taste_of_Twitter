8d48d404f502c3a282b274618b52f09c1e496e70
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-01-01 10:30:28 -0800

    add route for keyword search

--- back-end/main.py
+++ back-end/main.py
@@ -4,9 +4,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from keys import API_BEARER_TOKEN
 
-url = 'https://swapi.dev/api/people/1'
-
-# 'https://api.twitter.com/1.1/search/tweets.json?q=@levarburton'
+url = 'https://api.twitter.com/1.1/search/tweets.json?q=@'
 
 headers = {
     'Content-Type': 'application/json',
@@ -18,11 +16,20 @@ app = Flask(__name__)
 api = Api(app)
 cors= CORS(app, resources={r'/api/*': {'origins:': '*'}})
 
-@app.route('/api/tweets/search', methods=['GET'])
-def search_tweets():
-    response = requests.get(url, headers=headers).json()
-    print(response)
+@app.route('/api/tweets/search/<handle>', methods=['GET'])
+def search_tweets(handle):
+    response = requests.get(url+handle, headers=headers).json()
     return response
+
+@app.route('/api/tweets/search/<keyword>', methods=['GET'])
+def search_tweets(keyword):
+    response = requests.get(url+keyword, headers=headers).json()
+    return response
+
+# @app.errorhandler(404)
+# def not_found():
+#     """Page not found."""
+#     return make_response(render_template("404.html"), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
