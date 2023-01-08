795af21d34499d65521b155004556ed7e1b3c349
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-01-08 14:24:55 -0800

    relocate getProfileData and renderFavoriteProfiles functions

--- front-end/src/pages/Random.js
+++ front-end/src/pages/Random.js
@@ -29,44 +29,45 @@ const favoriteProfiles = [
     },
 ]
 
-useEffect (() => {
-    getProfileData();
-}, [])
-
-const getProfileData = async () => {
-    for (profile of favoriteProfiles) {
-        let path = profile;
-        const apiPath = `http://127.0.0.1:5000/api/tweets/random/${path}`
-        const response = await Axios.get(apiPath);
-        const profileData = response.data.statuses;
-        setProfiles(...profiles, profileData)
-    }
-}
-
-const renderFavoriteProfiles = () => {
-    return profiles.map((profile, i) => {
-        return(
-            <div className='container mt-5 col' key={i}>
-                <Headshot
-                    Name={profile.user.name}
-                    Source={profile.user}
-                    Description={profile.user.description}
-                    Alt={profile.user.name}
-                    />
-            </div>
-        )
-    })
-}
-
 const Random = () => {
 
+    useEffect (() => {
+        getProfileData();
+    }, [])
+    
     const [profiles, setProfiles] = useState([]);
- 
+
+    const getProfileData = async () => {
+
+        for (let i = 0; i <= favoriteProfiles.length; i++) {
+            let handle = favoriteProfiles[i].handle
+            const apiPath = `http://127.0.0.1:5000/api/tweets/random/${handle}`
+            const response = await Axios.get(apiPath);
+            const profileData = response.data.statuses;
+            setProfiles([...profiles, profileData])
+        }
+    }
+    
+    const renderFavoriteProfiles = () => {
+        return profiles.map((profile, i) => {
+            return(
+                <div className='container mt-5 col' key={i}>
+                    <Headshot
+                        Name={profile.user.name}
+                        Source={profile.user}
+                        Description={profile.user.description}
+                        Alt={profile.user.name}
+                        />
+                </div>
+            )
+        })
+    }
+    
     return (
         <div className='container'>
             <div className='container d-flex align-items-center'>
                 <div className='row'>
-                    {renderFavoriteProfiles}
+                    {renderFavoriteProfiles()}
                 </div>
             </div>
                 <div className='container d-flex align-items-center'>
