c6cbe1a421a649368cebb0e2e8979e08cad14eb0
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-01-25 21:41:54 -0800

    add styling

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -27,23 +27,23 @@ const Home = () => {
                     <div className='container search-container'>
                         <Link to={'/search'} style={{ textDecoration: 'none', color: 'inherit' }}>
                             <div className='card homepage-card shadow-lg text-bg-info mt-2 mb-2'>
-                                    <div className='card-body button-card text-bg-info'>
-                                        <div className='card-image-container'>
-                                            <img 
-                                                src={Search_Image}
-                                                alt='Search image'
-                                                className='img-thumbnail rounded shadow-sm card-image'
-                                            ></img>
-                                        </div>
-                                        <div className='card-text-container'>
-                                            <h3 className='card-title mb-2'>
-                                                Search
-                                            </h3>
-                                            <p>
-                                                Search for ten recent tweets from any Twitter user of your choice by @handle or keyword!
-                                            </p>
-                                        </div>
+                                <div className='card-body button-card text-bg-info'>
+                                    <div className='card-image-container'>
+                                        <img 
+                                            src={Search_Image}
+                                            alt='Search image'
+                                            className='img-thumbnail rounded shadow-sm card-image'
+                                        ></img>
                                     </div>
+                                    <div className='card-text-container'>
+                                        <h3 className='card-title mb-2'>
+                                            Search
+                                        </h3>
+                                        <p>
+                                            Search for ten recent tweets from any Twitter user of your choice by @handle or keyword!
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </Link>
                     </div>
@@ -51,23 +51,23 @@ const Home = () => {
                 <div className='col-6'>
                     <Link to={'/random'} style={{ textDecoration: 'none', color: 'inherit' }}>
                         <div className='card homepage-card shadow-lg text-bg-info mt-2 mb-2'>
-                                <div className='card-body button-card'>
-                                    <div className='card-image-container'>
-                                        <img 
-                                            src={Random_Image}
-                                            alt='Random tweet image'
-                                            className='img-thumbnail rounded-circle shadow-sm card-image'
-                                        ></img>
-                                    </div>
-                                    <div className='card-text-container'>
-                                        <h3 className='card-title mb-2'>
-                                            Random
-                                        </h3>
-                                        <p>
-                                            Get a random tweet from one of 5 users!
-                                        </p>
-                                    </div>
+                            <div className='card-body button-card'>
+                                <div className='card-image-container'>
+                                    <img 
+                                        src={Random_Image}
+                                        alt='Random tweet image'
+                                        className='img-thumbnail rounded-circle shadow-sm card-image'
+                                    ></img>
                                 </div>
+                                <div className='card-text-container'>
+                                    <h3 className='card-title text-muted mt-4 mb-2'>
+                                        Random
+                                    </h3>
+                                    <p>
+                                        Get a random tweet from one of 5 users!
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </Link>
                 </div>
