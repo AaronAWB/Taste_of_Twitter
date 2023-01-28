84bacc40cd4cfda934274e848215eb94db34a204
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-01-28 14:06:05 -0800

    remove Bootstrap grid from Home page

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -2,32 +2,38 @@ import { Link } from 'react-router-dom';
 import './Pages.css'
 import Random_Image from '../assets/images/Twitter_1.jpg';
 import Search_Image from '../assets/images/Search.jpg'
-import Header_Image from '../assets/images/Header.jpg'
+import Header_Image from '../assets/images/Orange.jpg'
 
 const Home = () => {
     return (  
         <div className='container'>
+            
+                
                     <div className='header-container'>
                         <div className='card header-card shadow-lg mt-2 mb-2'>
-                            <img 
-                                src={Header_Image} 
-                                class="card-img" 
-                                alt="sky image">
-                            </img>
+                            <div className='header-image-container'>
+                                <img 
+                                    src={Header_Image} 
+                                    className="header-image" 
+                                    alt="image of an orange">
+                                </img>
+                            </div>
                             <div className='card-body card-img-overlay header-card-body'>
-                                    <h1 className='card-title mb-2'>
-                                        Twitter Showcase
+                                    <h1 className='card-title main-header mb-2'>
+                                        Taste of Twitter
                                     </h1>
                                     <p>
-                                        This is an App that allows you to do two things:
-                                        1.  Search by Twitter Handle or Keyword to display 10 tweets; or
-                                        2.  Search for a random recent tweet from one of 5 pre-selected accounts.
+                                        Enjoy a small a small does of Twitter.
+                                        Search for recent tweets by user or keyword,
+                                        or explore random tweets from five
+                                        great pre-selected users. All without ever
+                                        logging in to your account.
                                     </p>
                             </div>
                         </div>
                     </div>
+                
                     <div className='button-card-container'>
-
                         <Link to={'/search'} style={{ textDecoration: 'none', color: 'inherit' }}>
                             <div className='card button-card shadow-lg mt-2 mb-2'>
                                 <div className='button-card-body'>
@@ -50,32 +56,32 @@ const Home = () => {
                                 </div>
                             </div>
                         </Link>
-                    
-                    <Link to={'/random'} style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <div className='card button-card shadow-lg mt-2 mb-2'>
-                            <div className='button-card-body'>
-                                <div className='card-image-container mt-3'>
-                                    <img 
-                                        src={Random_Image}
-                                        alt='Random tweet image'
-                                        className='img-thumbnail rounded-circle shadow-sm card-image'
-                                    ></img>
-                                </div>
-                                <div className='card-text-container'>
-                                    <h3 className='card-title mt-4 mb-2'>
-                                        Random
-                                    </h3>
-                                    <p>
-                                        Get a random tweet from one of 5 users!
-                                    </p>
+            
+                        <Link to={'/random'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <div className='card button-card shadow-lg mt-2 mb-2'>
+                                <div className='button-card-body'>
+                                    <div className='card-image-container mt-3'>
+                                        <img 
+                                            src={Random_Image}
+                                            alt='Random tweet image'
+                                            className='img-thumbnail rounded-circle shadow-sm card-image'
+                                        ></img>
+                                    </div>
+                                    <div className='card-text-container'>
+                                        <h3 className='card-title mt-4 mb-2'>
+                                            Random
+                                        </h3>
+                                        <p>
+                                            Get a random tweet from one of 5 users!
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Link>
-
-                </div>
-            
+                        </Link>
+                    </div>
+                        
         </div>
+        
     );
 }
  
